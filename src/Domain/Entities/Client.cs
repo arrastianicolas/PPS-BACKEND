724232b9bd410ce83b3b9dddaf9d1dbe31489ff7
mateocaranta <mateocaranta@gmail.com>
2024-08-23 20:05:59 -0300

    Entities creation

---
+++ src/Domain/Entities/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Client 
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string? FirstName { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string? LastName { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public int PhoneNumber { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+        [Required]
+        public string? Password { get; set; }    
+        
+    }
+}
