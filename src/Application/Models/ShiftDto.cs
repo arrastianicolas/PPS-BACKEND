9f177999ddd7196dea72e0f5b67bf7a51d7ba3e5
abranico <abranico011@gmail.com>
2024-10-04 19:35:06 -0300

    Add get and post shifts

---
+++ src/Application/Models/ShiftDto.cs
@@ -0,0 +1,36 @@
+﻿using Infrastructure.TempModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class ShiftDto
+    {
+
+        public DateTime Date { get; set; }
+
+        public int Idlocation { get; set; }
+
+        public string Dnitrainer { get; set; }
+
+        public int Peoplelimit { get; set; }
+
+        public List<int> ShiftsClientsId { get; set; }
+
+        public static ShiftDto Create(Shift shift)
+        {
+            return new ShiftDto
+            {
+                Date = shift.Date,
+                Idlocation = shift.Idlocation,
+                Dnitrainer = shift.Dnitrainer,
+                Peoplelimit = shift.Peoplelimit,
+                ShiftsClientsId = shift.Shiftsclients.Select(p => p.Idshift).ToList()
+            };
+
+        }
+    }
+}
---
+++ src/Web/Controllers/ShiftController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Application.Models.Requests;
+using Application.Interfaces;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize(Roles = "Admin")]
+    public class ShiftController : ControllerBase
+    {
+        private readonly IShiftService _shiftService;
+        public ShiftController(IShiftService shiftService)
+        {
+            _shiftService = shiftService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            return Ok(_shiftService.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult AddShift([FromBody] ShiftRequest shiftRequest)
+        {
+            var shiftDto = _shiftService.CreateShift(shiftRequest);
+            return Ok(shiftDto);
+        }
+    }
+}
