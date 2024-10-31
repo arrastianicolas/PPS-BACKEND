f0786c2c010fe220953e7cd0fa7d42b4aa2930e0
abranico <abranico011@gmail.com>
2024-10-31 04:58:57 -0300

    fix crud of nutritional plans

--- src/Application/Models/NutritionalPlanDto.cs
+++ src/Application/Models/NutritionalPlanDto.cs
@@ -9,7 +9,7 @@ namespace Application.Models
 {
     public class NutritionalPlanDto
     {
-        public int Id { get; set; }
+        public int IdNutritionalPlan { get; set; }
         public string DniClient { get; set; }
         public string DniTrainer { get; set; }
         public string Description { get; set; }
@@ -18,12 +18,16 @@ namespace Application.Models
         public string? Dinner { get; set; }
         public string? Brunch { get; set; }
         public string? Snack { get; set; }
+        public int IsPending { get; set; }
+        public string Weight { get; set; }
+        public string Height { get; set; }
+        public int IsActive { get; set; }
 
         public static NutritionalPlanDto Create(Nutritionalplan nutritionalPlan)
         {
             return new NutritionalPlanDto
             {
-                Id = nutritionalPlan.Idnutritionalplan,
+                IdNutritionalPlan = nutritionalPlan.Idnutritionalplan,
                 DniClient = nutritionalPlan.Dniclient,
                 DniTrainer = nutritionalPlan.Dnitrainer,
                 Description = nutritionalPlan.Description,
@@ -31,7 +35,11 @@ namespace Application.Models
                 Lunch = nutritionalPlan.Lunch,
                 Dinner = nutritionalPlan.Dinner,
                 Brunch = nutritionalPlan.Brunch,
-                Snack = nutritionalPlan.Snack
+                Snack = nutritionalPlan.Snack,
+                IsActive = nutritionalPlan.IsActive,
+                IsPending = nutritionalPlan.IsPending,
+                Weight = nutritionalPlan.Weight,
+                Height = nutritionalPlan.Height
             };
         }
     }
--- src/Application/Models/Requests/NutritionalPlanTrainerRequest.cs
+++ src/Application/Models/Requests/NutritionalPlanTrainerRequest.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 
 namespace Application.Models.Requests
 {
-    public class NutritionalPlanRequest
+    public class NutritionalPlanTrainerRequest
     {
-        public string Description { get; set; }
         public string? Breakfast { get; set; }
         public string? Lunch { get; set; }
         public string? Dinner { get; set; }
         public string? Brunch { get; set; }
         public string? Snack { get; set; }
+
+
+        // Si el entrenador quiere rechazar el plan, puede hacerlo enviando isActive como 0 y un mensaje aclarando el porque fue rechazado
+        public int? isActive { get; set; } = 1;
+        public string? Message { get; set; }
     }
 }
